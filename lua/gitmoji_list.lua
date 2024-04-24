d862fba3f4a9313e1efa992a25253c2e0555cf6e
Henrique Artur <contato@henriqueartur.com>
2024-04-23 22:05:46 -0300

    ✨ concatenation: emoji + description

---
+++ lua/gitmoji_list.lua
@@ -0,0 +1,8 @@
+local GITMOJI_LIST = require("lua.emoji_list")
+
+local GITMOJI_LIST_STRINGS = {}
+for _, emoji in ipairs(GITMOJI_LIST) do
+	table.insert(GITMOJI_LIST_STRINGS, emoji.emoji .. " " .. emoji.description)
+end
+
+return GITMOJI_LIST_STRINGS
