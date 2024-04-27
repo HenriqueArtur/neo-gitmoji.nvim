44d02409e4b2ca11dbd31ac309dc4f4a999b925b
Henrique Artur <contato@henriqueartur.com>
2024-04-27 19:26:44 -0300

    ♻️ refactor picker

--- lua/utils/gitmoji-picker.lua
+++ lua/utils/gitmoji-picker.lua
@@ -6,13 +6,7 @@ local conf = require("telescope.config").values
 local action_state = require("telescope.actions.state")
 local actions = require("telescope.actions")
 
-local GITMOJI_LIST = require("lua.gitmoji-list")
-
-local results_title = "| List |"
-local prompt_title = "| Chose a gitmoji |"
-local finder = finders.new_table({
-  results = GITMOJI_LIST,
-})
+local GITMOJI_LIST = require("lua.emoji-list")
 
 local default_opts = {
   layout_strategy = "vertical",
@@ -23,23 +17,44 @@ local default_opts = {
   },
 }
 
+local results_title = "| List |"
+local prompt_title = "| Chose a gitmoji |"
+
+local function get_gitmoji_list() return GITMOJI_LIST end
+
+---Format list to telescope
+---@param entry table A gitmoji item
+---@return table
+local function entry_maker(entry)
+  return {
+    value = entry,
+    display = entry.emoji .. " - " .. entry.description,
+    ordinal = entry.description,
+  }
+end
+
+local finder = finders.new_dynamic({
+  fn = get_gitmoji_list,
+  entry_maker = entry_maker,
+})
+
+local attach_mappings = function(prompt_bufnr)
+  return actions.select_default:replace(function()
+    actions.close(prompt_bufnr)
+    local selection = action_state.get_selected_entry()
+    return selection.value
+  end)
+end
+
 function M.open(opts)
   opts = opts or default_opts
-
   pickers
     .new(opts, {
       prompt_title = prompt_title,
       results_title = results_title,
       finder = finder,
       sorter = conf.generic_sorter(opts),
-      attach_mappings = function(prompt_bufnr)
-        actions.select_default:replace(function()
-          actions.close(prompt_bufnr)
-          local selection = action_state.get_selected_entry()
-          print(selection[1])
-        end)
-        return true
-      end,
+      attach_mappings = attach_mappings,
     })
     :find()
 end
