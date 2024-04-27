75764ad0485bee1460e8b29c6b189912f5746801
Henrique Artur <contato@henriqueartur.com>
2024-04-27 19:35:30 -0300

    ⚰️ remode gitmoji-list

--- lua/gitmoji-list.lua
+++
@@ -1,8 +0,0 @@
-local GITMOJI_LIST = require("lua.emoji-list")
-
-local GITMOJI_LIST_STRINGS = {}
-for _, emoji in ipairs(GITMOJI_LIST) do
-	table.insert(GITMOJI_LIST_STRINGS, emoji.emoji .. " - " .. emoji.description)
-end
-
-return GITMOJI_LIST_STRINGS
