f093c844f1f9bfff06d4bc4f41a3f8b6d912a973
Henrique Artur <contato@henriqueartur.com>
2024-04-27 21:55:23 -0300

    ✨ create startup function

--- lua/neo-gitmoji.lua
+++ lua/neo-gitmoji.lua
@@ -1,5 +1,19 @@
-vim.api.nvim_create_user_command("NeoGitmoji", function()
-  local package_name = "lua.utils.gitmoji-picker"
-  package.loaded[package_name] = nil
-  require(package_name).open_gitmoji_picker()
-end, {})
+local picker = require("lua.utils.gitmoji-picker").open_gitmoji_picker
+local floating_input = require("lua.utils.floating-input")
+
+local M = {}
+
+function M.setup_neo_gitmoji()
+  picker(function(value)
+    floating_input.create_floating_buffer({
+      title = "Enter the commit title: [gitmoji " .. value.emoji .. "]",
+      on_confirmation_callback = function(a_title)
+        print(value.emoji .. " " .. a_title)
+      end,
+    })
+  end)
+end
+
+vim.api.nvim_create_user_command("NeoGitmoji", function() M.setup_neo_gitmoji() end, {})
+
+return M
