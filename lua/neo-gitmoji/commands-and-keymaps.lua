bbd1462d56cd852152ae6fcba526b3e9e86e5f1b
Henrique Artur <contato@henriqueartur.com>
2024-04-28 20:39:10 -0300

    ✨ create set_keymaps

--- lua/neo-gitmoji/commands-and-keymaps.lua
+++ lua/neo-gitmoji/commands-and-keymaps.lua
@@ -6,4 +6,10 @@ function M.set_commands()
   vim.api.nvim_create_user_command("NeoGitmoji", function() M.setup() end, {})
 end
 
+function M.set_keymaps()
+  vim.keymap.set("n", "<leader>gm", function() start_neo_gitmoji() end, {
+    desc = "Gitmoji",
+  })
+end
+
 return M
