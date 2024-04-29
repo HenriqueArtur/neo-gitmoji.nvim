ef6ce2c93606f9dd35f7167b6f23960000195e2c
Henrique Artur <contato@henriqueartur.com>
2024-04-28 21:11:14 -0300

    ✨ run commit after select title

--- lua/neo-gitmoji.lua
+++ lua/neo-gitmoji.lua
@@ -1,4 +1,5 @@
 local picker = require("neo-gitmoji.gitmoji-picker").open_gitmoji_picker
+local Git = require("neo-gitmoji.git-commands")
 local floating_input = require("neo-gitmoji.floating-input")
 
 local M = {}
@@ -7,7 +8,7 @@ function M.setup()
   picker(function(value)
     floating_input.create_floating_buffer({
       title = "Enter the commit title: [gitmoji " .. value.emoji .. "]",
-      on_confirmation_callback = function(a_title) print(value.emoji .. " " .. a_title) end,
+      on_confirmation_callback = function(a_title) Git.commit(value.emoji .. " " .. a_title) end,
     })
   end)
 end
