1440aefbdc56927a836b2940f4da8269acea5736
Carlos vinicius <viniciusdev.26@gmail.com>
2025-01-27 12:53:38 -0300

    ♻️ move title format to function `format_title`

--- lua/neo-gitmoji/git-commands.lua
+++ lua/neo-gitmoji/git-commands.lua
@@ -1,8 +1,11 @@
 local Git = {}
 
+function format_title(title)
+  return title:gsub("`", "\\`")
+end
+
 function Git.commit(title)
-  local clean_title = title:gsub("`", "\\`")
-  vim.cmd(string.format('!git commit -m "%s"', clean_title))
+  vim.cmd(string.format('!git commit -m "%s"', format_title(title)))
 end
 
 return Git
