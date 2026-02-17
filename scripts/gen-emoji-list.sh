44480cfe7f1aceb3504d1b65ca325f6b70e57c78
Henrique Artur <contato@henriqueartur.com>
2026-02-17 11:25:12 -0300

    📦️ update emoji list

--- scripts/gen-emoji-list.sh
+++ scripts/gen-emoji-list.sh
@@ -4,23 +4,19 @@ destination_folder="./lua/neo-gitmoji/"
 file="${destination_folder}emoji-list.lua"
 tmp_file="${destination_folder}emoji-list-tmp.lua"
 
-almost_parsed=$(curl https://raw.githubusercontent.com/carloscuesta/gitmoji/master/packages/gitmojis/src/gitmojis.json \
+curl -s https://raw.githubusercontent.com/carloscuesta/gitmoji/master/packages/gitmojis/src/gitmojis.json \
 | jq .gitmojis \
-| sed 's/\"\(\w*\)\"\:/\1\ \=/' \
-| sed 's/null/nil/')
-
-last_line=$(wc -l <$almost_parsed)
-echo "$almost_parsed" \
-| sed '1s/\[/return\ \{/' \
-| sed "${last_line}s/\]/\}/" \
->> $tmp_file
+| sed 's/"\([a-zA-Z_][a-zA-Z0-9_]*\)":/\1 =/' \
+| sed 's/null/nil/' \
+| sed '1s/\[/return {/' \
+| sed '$s/\]/}/' \
+> $tmp_file
 
 if [ -f $tmp_file ]; then
-	if cmp "$file" "$tmp_file"; then
+	if cmp -s "$file" "$tmp_file"; then
 		rm $tmp_file
 	else
 		rm $file
 		mv $tmp_file $file
 	fi
 fi
-
